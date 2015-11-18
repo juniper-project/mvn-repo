e71edd44c94ee393cfe75fee0a2727483c535908
Alexey <hpcochep@Pupsik.(none)>
2015-11-18 13:17:00 +0100

    Update

--- deploy_to_local_mvn.sh
+++ deploy_to_local_mvn.sh
@@ -1,1 +1,7 @@
-mvn install:install-file -Dfile=/home/hpcochep/Projects/Running/JUNIPER/Development/GIT/mvn-repo/openmpi-1-0.jar -DgroupId=org.openmpi -DartifactId=openmpi -Dversion=1.0 -Dpackaging=jar -DlocalRepositoryPath=https://github.com/juniper-project/mvn-repo.git
+mvn install:install-file \
+ -DgroupId=org.openmpi \
+ -DartifactId=openmpi \
+ -Dversion=1.0 \
+ -Dpackaging=jar \
+ -Dfile=/home/hpcochep/Projects/Running/JUNIPER/Development/GIT/mvn-repo/openmpi-1-0.jar \
+ -DlocalRepositoryPath=/home/hpcochep/Projects/Running/JUNIPER/Development/GIT/mvn-local-repo
